3505d4b3f730da45233bee96693de94257af76f5
zhuchenyu2008 <zhuchenyu2008@foxmail.com>
2025-01-18 09:21:07 +0800

    1

--- new.py
+++ new.py
@@ -4,6 +4,19 @@ time.sleep(1)
 i=input("Are you ready to strat?(y/n)")
 if i=='y':
     print("Let's start")
+    print("give you a lot of weclome!")
 else:
     print("Goodbye")
     exit()
+
+a = 100
+while 0 < a:
+    print("weclome")
+    a=a-1
+    time.sleep(0.1)
+    if 1<a:
+        print(a-1,"left")
+    else:
+        print("so far")
+
+
