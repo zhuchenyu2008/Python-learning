dd052d970e5895b0e2cfee6adeea26c098717db8
zhuchenyu2008 <zhuchenyu2008@foxmail.com>
2025-01-18 09:07:17 +0800

    1

---
+++ new.py
@@ -0,0 +1,9 @@
+import time
+print ("this is a new world of python")
+time.sleep(1)
+i=input("Are you ready to strat?(y/n)")
+if i=='y':
+    print("Let's start")
+else:
+    print("Goodbye")
+    exit()
