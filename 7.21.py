a87fd2c35c991b46c91f9b9391f4db61a96fd2cf
zhuchenyu2008 <zhuchenyu2008@foxmail.com>
2025-07-21 08:38:54 +0800

    1

---
+++ 7.21.py
@@ -0,0 +1,42 @@
+import random
+b = random.randint(1,100)
+c = False
+e = 0
+while not c:
+    a = eval(input("输入数字:>>"))
+    if a == b:
+        print("猜对了")
+        c = True
+    elif a > b :
+        print("猜大了")
+        c = False
+        e = e + 1
+
+        if a-b < 10 :
+            print("但是离答案很近了")
+
+        if e > 10:
+            d = input("你已经猜了%s次还要继续吗？（y/n） >>" % e)
+            if d == 'y':
+                c = False
+            else:
+                c = True
+
+    elif a < b :
+        print("猜小了")
+        c = False
+        e = e + 1
+
+        if b-a < 10:
+            print("但是离答案很近了")
+
+        if e > 10:
+            d = input("你已经猜了%s次还要继续吗？（y/n） >>" % e)
+            if d == 'y':
+                c = False
+            else:
+                c = True
+
+if c == True:
+    print("答案是：",b)
+
---
+++ 7.21（2）.py
@@ -0,0 +1,52 @@
+a=input("注册/登录 （1/2）： >>")
+user_name="admin"
+user_password="admin"
+
+if a == '1':
+    print("注册程序启动")
+    mistake=True
+    while mistake==True:
+
+        user_name=input("输入用户名： >>")
+        user_password=input("输入密码： >>")
+        user_password2=input("确认密码： >>")
+
+        if user_password == user_password2:
+            print("注册成功")
+            mistake = False
+
+            user_choice=input("是否进入登录程序？ (y/n) >>")
+            if user_choice == 'y':
+                a="2"
+                break
+            else:
+                print("已退出")
+                exit()
+        else:
+            print("两次密码不同，请重试")
+
+
+if a == '2':
+    print("登录程序启动")
+    mistake = True
+    while mistake == True:
+        user_name_login=input("输入用户名： >>")
+        user_password_login=input("输入密码： >>")
+        if user_name_login==user_name and user_password_login==user_password:
+            print("登录成功")
+            if user_name=="admin":
+                print("管理员登录")
+                mistake = False
+            else:
+                print("欢迎你，%s" %user_name)
+                mistake = False
+        else:
+            print("用户名或密码错误")
+            mistake = True
+
+
+else:
+    print("输入错误")
+
+
+
